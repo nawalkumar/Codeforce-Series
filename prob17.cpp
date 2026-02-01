e41e49d6f9b7c239183f0769a92c9ee562d2805d
nawalkumar <156170708+nawalkumar@users.noreply.github.com>
2026-02-02 01:00:55 +0530

    Complex Query- Reverse Xor

--- prob17.cpp
+++ prob17.cpp
@@ -1,34 +1,76 @@
 #include<bits/stdc++.h>
 using namespace std;
-int reverse(int n)
+int countSetBits(int n)
 {
-    int rev=0;
+    int count=0;
     while(n)
     {
-        rev=(rev<<1)|(n&1);
+        count+=n&1;
         n=n>>1;
     }
-    return rev;
+    return count;
 }
+
+void solve(){
+    int n;
+    cin>>n;
+    if(n==0){
+        cout<<"YES\n";
+        return;
+    }
+    if(countSetBits(n)%2!=0){
+        cout<<"NO"<<"\n";
+        return;
+    }
+
+    string s="";
+    for(int i=29;i>=0;i--)
+    {
+        if(n&(1<<i)){
+            for(int j=i;j>=0;j--)
+            {
+                s+=(n&(1<<j))?'1':'0';
+            }
+            break;
+        }
+    }
+    int zeros=0;
+    while(zeros<s.length() && s[s.length()-1-zeros]=='0'){
+        zeros++;
+    }
+    for(int k=0;k<=zeros;k++)
+    {
+        string temp=string(k,'0')+s;
+        string rev_t=temp;
+        reverse(rev_t.begin(),rev_t.end());
+        if(temp==rev_t)
+        {
+            if(temp.size()%2!=0)
+            {
+                if(temp[temp.size()/2]=='0')
+                {
+                    cout<<"YES\n";
+                    return;
+                }
+            }
+            else{
+                cout<<"YES\n";
+                return;
+            }
+        }
+    }
+    cout<<"NO\n";   
+}
+
+
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     int t;
     cin>>t;
-    bool ok=false;
     while(t--){
        int n;
        cin>>n;
-       for(int x=1;x<(1<<15);x++)
-       {
-            if(x^reverse(x)==n)
-            {
-                ok=true;
-                break;
-            }
-       }
-       if(ok) cout<<"YES\n";
-       else cout<<"NO\n";
-        ok=false;
+       solve();
     }
 }
